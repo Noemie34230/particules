6beca4e98483dda85bd6fedbb8b060213a21c8c2
Noémie Mateu <noemiemateu@yahoo.fr>
2023-09-27 16:41:05 +0200

    définition des limites du canvas, mise en place de l'opacité sur les rayons du cercle, nettoyage du code et commentaires

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,15 @@ import "./style.css";
 const canvas = document.querySelector<HTMLCanvasElement>("#particules-canvas")!;
 
 const ctx = canvas.getContext("2d")!;
-ctx.fillStyle = "#000000";
+
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
 
-
+// création du fond noir qui fait toute la taille de l'écran
 
 canvas.style.background = "#000000";
 ctx.fillRect(0, 0, width, height);
+
 
 // création de variables globales
 
@@ -20,34 +21,10 @@ let positionY: number;
 // création d'un tableau vide pour pouvoir stocker les différents cercles par la suite
 
 const arrayCircle: Circle[] = [];
-
-
 let audioContext = new (window.AudioContext || (window as any).webkitAudioContext)();
 
 
-// Création de l'oscillateur
-const oscillator = audioContext.createOscillator();
-oscillator.type = "square"; // Valeur possible (sine, square, triangle, sawtooth)
-oscillator.frequency.setValueAtTime(340, audioContext.currentTime);
-
-// Connexion de l'oscillateur à la destination audio
-oscillator.connect(audioContext.destination);
-
-// Fonction pour démarrer la chute de fréquence
-function startChute() {
-    const currentTime = audioContext.currentTime;
-    oscillator.frequency.setValueAtTime(340, currentTime); // Définir la fréquence de départ
-
-    const dureeChute = 3.0; // Durée de la chute en secondes
-    const frequence = 0; // Fréquence à zéro pour ne plus rien entendre
-
-
-    // Définir la fréquence minimale à la fin de la chute
-    oscillator.frequency.linearRampToValueAtTime(frequence, currentTime + dureeChute);
-}
-
-
-// La création d'une class Circle permet d'avoir un "moule " pour que tous les cercles soient pareils
+// La création d'une class Circle 
 export class Circle {
     positionInitialeX: number;
     positionInitialeY: number;
@@ -56,12 +33,13 @@ export class Circle {
     color: any;
     radius: number;
 
-    //   Le constructeur est comme la "recette" qu'on doit suivre 
+
+    //   Le constructeur de la class Circle
     constructor(
         positionInitialeX: number,
         positionInitialeY: number,
         nouvellePositionX: number,
-        nouvellePositionY: number
+        nouvellePositionY: number,
     ) {
         this.positionInitialeX = positionInitialeX;
         this.positionInitialeY = positionInitialeY;
@@ -69,6 +47,7 @@ export class Circle {
         this.nouvellePositionY = nouvellePositionY;
         this.color = this.generateColor();
         this.radius = this.generateRadius();
+
     }
 
     //   Méthode pour permettre de créer une couleur aléatoire des cercles
@@ -78,45 +57,77 @@ export class Circle {
 
     //   Méthode pour permettre de créer un rayon aléatoire des cercles
     generateRadius() {
-        return Math.floor(Math.random() * 100);
+        return Math.floor(Math.random() * 20);
     }
 
     //   Méthode pour créer du mouvement (la position initiale devient la nouvelle position )
     movement() {
+
+        
         this.positionInitialeX += this.nouvellePositionX;
         this.positionInitialeY += this.nouvellePositionY;
+
     }
 
-    //   Méthode pour créer un cercle - utilisation de l'API canvas
+    //   Méthode pour dessiner un cercle - utilisation de l'API canvas
     drawCircle(ctx: CanvasRenderingContext2D) {
-        
-        // for (let i = 0; i < 7; i++) {
-        ctx.beginPath();
-        ctx.fillStyle = this.color;
-        
-        ctx.arc(this.positionInitialeX, this.positionInitialeY, this.radius , 0, 2 * Math.PI);
+        ctx.globalAlpha = 0.3
+        for (let i = 0; i < 5; i++) {
+            ctx.beginPath();
+            ctx.fillStyle = this.color;
+            ctx.arc(this.positionInitialeX, this.positionInitialeY, this.radius * i, 0, 2 * Math.PI);
+            ctx.fill();
+            ctx.closePath();
+            ctx.beginPath();
 
-        ctx.fill();
-        ctx.closePath();
-        // }
+        }
     }
+
 }
+
+
+
+
+// Création de l'oscillateur
+const oscillator = audioContext.createOscillator();
+oscillator.type = "square"; // Valeur possible (sine, square, triangle, sawtooth)
+oscillator.frequency.setValueAtTime(300, audioContext.currentTime);
+
+// Connexion de l'oscillateur à la destination audio
+oscillator.connect(audioContext.destination);
+
+// Fonction pour démarrer la chute de fréquence
+function startChute() {
+    const currentTime = audioContext.currentTime;
+    oscillator.frequency.setValueAtTime(300, currentTime); // Définir la fréquence de départ
+
+    const dureeChute = 1.0; // Durée de la chute en secondes
+    const frequence = 0; // Fréquence à zéro pour ne plus rien entendre
+
+
+    // Définir la fréquence minimale à la fin de la chute
+    oscillator.frequency.linearRampToValueAtTime(frequence, currentTime + dureeChute);
+}
+
+
 
 // Création d'une fonction pour générer un nouveau cercle
 function createNewCircle() {
     const angle = Math.random() * Math.PI * 2;
     const distance = Math.random() * 10;
-
-    // ctx.globalAlpha = Math.random();
     let nouvellePositionX = Math.cos(angle) * distance;
     let nouvellePositionY = Math.sin(angle) * distance;
+
 
     const circle = new Circle(
         positionX,
         positionY,
         nouvellePositionX,
-        nouvellePositionY
+        nouvellePositionY,
+
     );
+
+
     //  Cela permet de stocker les cercles dans le tableau de cercles
     arrayCircle.push(circle);
 
@@ -125,29 +136,32 @@ function createNewCircle() {
     let soundActivated = false;
 
     canvas.addEventListener("click", (e) => {
-        
+
         e.preventDefault;
-        
+
         positionX = e.clientX;
         positionY = e.clientY;
         createNewCircle();
+
+
         // Si le son n'a pas encore été activé, activez-le
         if (!soundActivated) {
             startChute();
             soundActivated = true;
         }
-
         oscillator.start(audioContext.currentTime);
         startChute();
-
 
     });
 
 }
 
-// Boucle pour créer de nouveaux cercles
-for (let i = 0; i < 20; i++) {
+
+// Boucle pour créer de 10 nouveaux cercles
+for (let i = 0; i < 10; i++) {
     createNewCircle();
+
+
 }
 
 // Fonction pour mettre à jour et dessiner les cercles
@@ -156,15 +170,25 @@ function updateAndDrawCircles() {
 
     for (const circle of arrayCircle) {
         circle.movement();
+
         circle.drawCircle(ctx);
+
+
+        // Vérifiez les limites du canvas
+        if (circle.positionInitialeX < 0 || circle.positionInitialeX > width - 50) {
+            circle.positionInitialeX = width / 2;
+        }
+        if (circle.positionInitialeY < 0 || circle.positionInitialeY > height - 50) {
+            circle.positionInitialeY = height / 2;
+        }
+
     }
+    requestAnimationFrame(updateAndDrawCircles); // fonction spécifique de mise à jour de l'animation, avant le prochain rafraîchissement à l'écran du navigateur
 
-
-
-    requestAnimationFrame(updateAndDrawCircles); // Appel récursif pour l'animation
 }
 
 updateAndDrawCircles(); // Démarrer l'animation
+
 
 
 
